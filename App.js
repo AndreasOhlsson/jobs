d53f5a5ed3bb8861a12261ee7b5b4d2464427653
Andreas Ohlsson <andreasxohlsson@gmail.com>
2018-03-23 22:50:37 +0100

    added screens

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation'
+import AuthScreen from './screens/AuthScreen'
+import WelcomeScreen from './screens/WelcomeScreen'
+import MapScreen from './screens/MapScreen'
+import DeckScreen from './screens/DeckScreen'
+import SettingsScreen from './screens/SettingsScreen'
+import ReviewScreen from './screens/ReviewScreen'
 
-export default class App extends React.Component {
+class App extends Component {
   render() {
+    const MainNavigator = TabNavigator({
+      welcome: { screen: WelcomeScreen },
+      auth: { screen: AuthScreen },
+    })
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <MainNavigator />
       </View>
     );
   }
@@ -17,5 +33,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+});
+
+export default TabNavigator({
+  welcome: { screen: WelcomeScreen },
+  auth: { screen: AuthScreen },
+  main: {
+    screen: TabNavigator({
+      map: { screen: MapScreen },
+      deck: { screen: DeckScreen },
+      rewiew: {
+        screen: StackNavigator({
+          rewiew: { screen: ReviewScreen },
+          settings: { screen: SettingsScreen },
+        })
+      }
+    })
   },
 });
