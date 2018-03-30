6a5e76d26361012d97a43f081a23b65e70447686
Andreas Ohlsson <andreasxohlsson@gmail.com>
2018-03-30 23:56:25 +0200

    added jobsreducer

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -3,17 +3,60 @@ import {
   View,
   Text,
 } from 'react-native'
+import { MapView } from 'expo'
+import { connect } from 'react-redux'
+import { Button } from 'react-native-elements'
+import * as actions from '../actions'
 
 class MapScreen extends Component {
+  state = {
+    region: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+      longitudeDelta: 0.0421,
+      latitudeDelta: 0.0922,
+    }
+  }
+
+  onRegionChangeComplete = (region) => {
+    this.setState({ region })
+  }
+
+  onButtonPress = () => {
+    this.props.fetchJobs(this.state.region, () => {
+      this.props.navigation.navigate('deck')
+    })
+  }
+
   render() {
     return (
-      <View>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
+      <View style={{ flex: 1 }}>
+        <MapView
+          style={{ flex: 1 }}
+          region={this.state.region}
+          onRegionChangeComplete={this.onRegionChangeComplete}
+        />
+        <View style={styles.buttonContainer}>
+          <Button
+            large
+            title="Search this area"
+            backgroundColor="#009688"
+            icon={{ name: 'search' }}
+            onPress={this.onButtonPress}
+          />
+        </View>
       </View>
     )
   }
 }
 
-export default MapScreen
+const styles = {
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    right: 0,
+  }
+}
+
+export default connect(null, actions)(MapScreen)
